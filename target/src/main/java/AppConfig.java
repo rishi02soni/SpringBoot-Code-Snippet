0e5d54ab702dc4f86190710258ae6a559a3e2535
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-08-07 21:02:43 +0530

    Create AppConfig.java

---
+++ target/src/main/java/AppConfig.java
@@ -0,0 +1,12 @@
+package in.coderarmy;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("in.rishisoni")
+public class AppConfig {
+
+
+}
