62e60227c4becaaadc7ade1a4d93e8a6c33be715
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-08-07 21:03:02 +0530

    Update Main.java

--- target/src/main/java/Main.java
+++ target/src/main/java/Main.java
@@ -1,12 +1,22 @@
 package in.coderarmy;
 
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("in.soniRishi")
-public class AppConfig {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
+public class Main {
+
+    public static void main(String[] args) {
+
+       ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+       //is line ka meaning hai ki ek spring container ko start kro using annotation based config.
+       // rules batane hoge container ko sare rules batane honge , how to config , kaun kaun se class ko monitor
+       // ye rule alag class mein batayenge aur us class ki reflection ko pass krenge applicationcontetx ke paramnte rmein..
+
+        OrderService order = context.getBean(OrderService.class);
+        order.placeOrder();
+
+
+
+    }
 }
