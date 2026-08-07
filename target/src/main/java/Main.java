5dee7fa0445c4d8e1f6484520f6e5e59d6003350
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-08-07 21:01:37 +0530

    Create Main.java

---
+++ target/src/main/java/Main.java
@@ -0,0 +1,12 @@
+package in.coderarmy;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("in.soniRishi")
+public class AppConfig {
+
+
+}
